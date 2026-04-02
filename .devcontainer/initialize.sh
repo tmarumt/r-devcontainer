4d8d9b8052ea882731cb0936ce2ac417d04fbac2
tmarumt <tmarumt1@gmail.com>
2026-04-02 21:40:09 +0900

    feat: git config local for subdir

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -8,14 +8,24 @@ for vol in ${volumes[@]}; do
   fi
 done
 
+repositories=("./")
+
+for obj in ./personal/*/; do
+  if [ -d "$obj" ] && [ "$(git -C "$obj" rev-parse --git-dir)" == ".git" ]; then
+    repositories+=("$obj")
+  fi
+done
+
 configurations=("user.name" "user.email")
 
-for config in ${configurations[@]}; do
-  cur_config="$(git config $config)"
-  global_config="$(git config --global $config)"
-  local_config="$(git config --local $config)"
+for repo in ${repositories[@]}; do
+  for config in ${configurations[@]}; do
+    cur_config="$(git -C "$repo" config --get $config)"
+    global_config="$(git -C "$repo" config --global --get $config)"
+    local_config="$(git -C "$repo" config --local --get $config)"
 
-  if [ -z "$local_config" ] && [ -n "$cur_config" ] && [ "$cur_config" != "$global_config" ]; then
-    git config --local $config "$cur_config"
-  fi
+    if [ -z "$local_config" ] && [ -n "$cur_config" ] && [ "$cur_config" != "$global_config" ]; then
+      git -C "$repo" config --local $config "$cur_config"
+    fi
+  done
 done
